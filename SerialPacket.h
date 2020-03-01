d8023c0e3b72931197ffe6cb753d53603a956d9a
emzarem <emurphyzaremba@gmail.com>
2020-03-01 16:41:25 -0500

    Added cmd type to allow for end effector operation

--- SerialPacket.h
+++ SerialPacket.h
@@ -6,32 +6,48 @@
 
 namespace SerialUtils {
 
+static const int MAX_SUPPORTED_MTRS = 3;
+
 static const char DELIMITER = '\n';
 
+enum CmdType {
+    CMDTYPE_MTRS = 0,          // Actuate motors
+    CMDTYPE_CAL,               // Calibrate motors
+    CMDTYPE_ENDEFF_ON,         // Turn ee on
+    CMDTYPE_ENDEFF_OFF,        // Turn ee off
+    CMDTYPE_RESP
+};
+
 struct CmdMsg {
+    /* Type of message */
+    CmdType cmd_type;
+
     /* Data to send to Teensy */
     bool is_relative;
-    float m1_angle, m2_angle, m3_angle;
+    float mtr_angles[3];
+    
     /* Data to be received from Teensy */
-    bool motors_done;
+    bool cmd_success;
 
+    /* operators */
     operator std::string() const
     {
         std::ostringstream oss;
         oss << "CmdMsg " << this << ":" << std::endl
+            << "    cmd_type: " << this->cmd_type << std::endl
             << "    is_relative: " << this->is_relative << std::endl
-            << "    m1_angle: " << this->m1_angle << std::endl
-            << "    m2_angle: " << this->m2_angle << std::endl
-            << "    m3_angle: " << this->m3_angle << std::endl
-            << "    motors_done: " << this->motors_done << std::endl;
+            << "    m1_angle: " << this->mtr_angles[0] << std::endl
+            << "    m2_angle: " << this->mtr_angles[1] << std::endl
+            << "    m3_angle: " << this->mtr_angles[2] << std::endl
+            << "    cmd_success: " << this->cmd_success << std::endl;
         return oss.str();
     }
-};
+} __attribute__((packed));
 
 union Packet {
     CmdMsg msg;
     char bytes[sizeof(CmdMsg)];
-} __attribute__((packed));
+};
 
 inline void pack(std::vector<char> &dest, CmdMsg &src) {
     Packet pkt = {.msg = src};
